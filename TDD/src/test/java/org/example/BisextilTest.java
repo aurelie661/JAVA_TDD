b9bdba97fd11f674d5ec67e5d1ef7f994445a590
aurelie661 <boutaurel59@gmail.com>
2023-08-24 15:12:22 +0200

    exo3 Test unitaire amélioré

--- TDD/src/test/java/org/example/BisextilTest.java
+++ TDD/src/test/java/org/example/BisextilTest.java
@@ -1,20 +1,23 @@
 package org.example;
-
 import org.example.entity.Bisextil;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class BisextilTest {
     private Bisextil bisextil;
+
+    @BeforeEach
+    public void setUp(){
+        bisextil = new Bisextil();
+    }
     @Test
     public void testYearBisextilShouldBeTrueWhenYearIs2000(){
-        bisextil =   new Bisextil();
         boolean result = bisextil.bisextil(2000);
         Assertions.assertTrue(result);
     }
     @Test
     public void testYearBisextilShouldBeFalseWhenYearIs2023(){
-        bisextil =   new Bisextil();
         boolean result = bisextil.bisextil(2023);
         Assertions.assertFalse(result);
     }
