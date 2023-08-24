e4017a5fc1d439d21f1e71fcf6c43026f6eb548f
aurelie661 <boutaurel59@gmail.com>
2023-08-24 11:52:22 +0200

    exo Test unitaire amélioré

--- TDD/src/test/java/org/example/GradingCalculatorTest.java
+++ TDD/src/test/java/org/example/GradingCalculatorTest.java
@@ -1,71 +1,97 @@
 package org.example;
-
 import org.example.entity.GradingCalculator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class GradingCalculatorTest {
+    private GradingCalculator gradingCalculator;
+    private void initGradingCalculator(int score, int attendance) {
+        gradingCalculator = new GradingCalculator();
+        gradingCalculator.setScore(score);
+        gradingCalculator.setAttendancePercentage(attendance);
+    }
     @Test
     public void getGradeTestScore95andShouldBePercentage90(){
-        GradingCalculator gradingCalculator = new GradingCalculator();
+        /*GradingCalculator gradingCalculator = new GradingCalculator();
         gradingCalculator.setScore(95);
-        gradingCalculator.setAttendancePercentage(90);
+        gradingCalculator.setAttendancePercentage(90);*/
 
-        char c = gradingCalculator.getGrade();
-        Assertions.assertEquals('A',c);
+        initGradingCalculator(95,90);
+
+        char result  = gradingCalculator.getGrade();
+
+        Assertions.assertEquals('A', result);
     }
     @Test
     public void getGradeTestScore85andShouldBePercentage90(){
-        GradingCalculator gradingCalculator = new GradingCalculator();
+       /* GradingCalculator gradingCalculator = new GradingCalculator();
         gradingCalculator.setScore(85);
-        gradingCalculator.setAttendancePercentage(90);
+        gradingCalculator.setAttendancePercentage(90);*/
 
-        char c = gradingCalculator.getGrade();
-        Assertions.assertEquals('B',c);
+        initGradingCalculator(85,90);
+
+        char result  = gradingCalculator.getGrade();
+
+        Assertions.assertEquals('B', result);
     }
     @Test
     public void getGradeTestScore65andShouldBePercentage90(){
-        GradingCalculator gradingCalculator = new GradingCalculator();
+        /*GradingCalculator gradingCalculator = new GradingCalculator();
         gradingCalculator.setScore(65);
-        gradingCalculator.setAttendancePercentage(90);
+        gradingCalculator.setAttendancePercentage(90);*/
 
-        char c = gradingCalculator.getGrade();
-        Assertions.assertEquals('C',c);
+        initGradingCalculator(65,90);
+
+        char result  = gradingCalculator.getGrade();
+
+        Assertions.assertEquals('C', result);
     }
     @Test
     public void getGradeTestScore95andShouldBePercentage65(){
-        GradingCalculator gradingCalculator = new GradingCalculator();
+        /*GradingCalculator gradingCalculator = new GradingCalculator();
         gradingCalculator.setScore(95);
-        gradingCalculator.setAttendancePercentage(65);
+        gradingCalculator.setAttendancePercentage(65);*/
 
-        char c = gradingCalculator.getGrade();
-        Assertions.assertEquals('B',c);
+        initGradingCalculator(95,65);
+
+        char result  = gradingCalculator.getGrade();
+
+        Assertions.assertEquals('B', result);
     }
     @Test
     public void getGradeTestScore95andShouldBePercentage55(){
-        GradingCalculator gradingCalculator = new GradingCalculator();
+        /*GradingCalculator gradingCalculator = new GradingCalculator();
         gradingCalculator.setScore(95);
-        gradingCalculator.setAttendancePercentage(55);
+        gradingCalculator.setAttendancePercentage(55);*/
 
-        char c = gradingCalculator.getGrade();
-        Assertions.assertEquals('F',c);
+        initGradingCalculator(95,55);
+
+        char result  = gradingCalculator.getGrade();
+
+        Assertions.assertEquals('F', result);
     }
     @Test
     public void getGradeTestScore65andShouldBePercentage55(){
-        GradingCalculator gradingCalculator = new GradingCalculator();
+        /*GradingCalculator gradingCalculator = new GradingCalculator();
         gradingCalculator.setScore(65);
-        gradingCalculator.setAttendancePercentage(55);
+        gradingCalculator.setAttendancePercentage(55);*/
 
-        char c = gradingCalculator.getGrade();
-        Assertions.assertEquals('F',c);
+        initGradingCalculator(65,55);
+
+        char result  = gradingCalculator.getGrade();
+
+        Assertions.assertEquals('F', result);
     }@Test
     public void getGradeTestScore50andShouldBePercentage90(){
-        GradingCalculator gradingCalculator = new GradingCalculator();
+        /*GradingCalculator gradingCalculator = new GradingCalculator();
         gradingCalculator.setScore(50);
-        gradingCalculator.setAttendancePercentage(90);
+        gradingCalculator.setAttendancePercentage(90);*/
 
-        char c = gradingCalculator.getGrade();
-        Assertions.assertEquals('F',c);
+        initGradingCalculator(50,90);
+
+        char result  = gradingCalculator.getGrade();
+
+        Assertions.assertEquals('F', result);
     }
 
 }
