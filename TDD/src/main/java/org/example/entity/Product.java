056282ea9c70b8f7356110d6dadced2372141668
aurelie661 <boutaurel59@gmail.com>
2023-08-25 16:12:18 +0200

    exo5 Test unitaire

--- TDD/src/main/java/org/example/entity/Product.java
+++ TDD/src/main/java/org/example/entity/Product.java
@@ -1,28 +1,13 @@
 package org.example.entity;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class Product {
     private int sellIn;
     private int quality;
-
-    public int getSellIn() {
-        return sellIn;
-    }
-
-    public void setSellIn(int sellIn) {
-        this.sellIn = sellIn;
-    }
-
-    public int getQuality() {
-        return quality;
-    }
-
-    public void setQuality(int quality) {
-        this.quality = quality;
-    }
-    public int updateProduct(int sellIn,int quality){
-        if(sellIn <= 0){
-            setQuality(--quality);
-        }
-        return sellIn;
-    }
+    private String name;
+    private String type;
 }
