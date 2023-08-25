a6d4db08d3c6ca6fdde162729485c031a871541c
aurelie661 <boutaurel59@gmail.com>
2023-08-25 16:13:46 +0200

    exo5 Test unitaire

---
+++ TDD/src/main/java/org/example/entity/Shop.java
@@ -0,0 +1,49 @@
+package org.example.entity;
+
+import org.example.exception.MaxQualityException;
+import org.example.exception.NegativeQualityException;
+
+public class Shop {
+    public void update(Product product){
+        int minusQuality = 0;
+        if(product.getQuality() <= 0) {
+            throw new NegativeQualityException();
+        }
+        if(product.getQuality() > 50) {
+            throw new MaxQualityException();
+        }
+        if(product.getType() != null && product.getType().equals("laitier")) {
+            if(product.getName() != null && product.getName().equals("brie vieilli")) {
+                minusQuality = -1;
+            }else {
+                if(product.getSellIn() == 0) {
+                    if(product.getQuality()>3)
+                        minusQuality = 4;
+                    else
+                        minusQuality = product.getQuality();
+                }
+                else {
+                    if(product.getQuality() > 1)
+                        minusQuality = 2;
+                    else
+                        minusQuality = product.getQuality();
+                }
+
+            }
+        }
+
+        else {
+            if(product.getSellIn() <= 0) {
+                if(product.getQuality() == 1){
+                    minusQuality = product.getQuality();
+                }else {
+                    minusQuality = 2;
+                }
+            }else {
+                minusQuality = 1;
+            }
+        }
+        product.setQuality(product.getQuality() - minusQuality);
+        product.setSellIn(product.getSellIn()-1);
+    }
+}
---
+++ TDD/src/test/java/org/example/ShopTest.java
@@ -0,0 +1,112 @@
+package org.example;
+import org.example.entity.Product;
+import org.example.entity.Shop;
+import org.example.exception.MaxQualityException;
+import org.example.exception.NegativeQualityException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ShopTest {
+
+    private Shop shop;
+    private Product product;
+
+    @BeforeEach
+    void setUp() {
+        shop = new Shop();
+    }
+
+    @Test
+    void testUpdateShouldDecreaseQuality() {
+        //Arrange
+        product = Product.builder().quality(10).sellIn(10).build();
+
+        //Act
+        shop.update(product);
+
+        //Assert
+        Assertions.assertEquals(9, product.getQuality());
+
+    }
+
+    @Test
+    void testUpdateShouldDecreaseSellIn() {
+        //Arrange
+        product = Product.builder().quality(10).sellIn(10).build();
+
+        //Act
+        shop.update(product);
+
+        //Assert
+        Assertions.assertEquals(9, product.getSellIn());
+
+    }
+
+
+    @Test
+    void testUpdateShouldDecreaseQualityTwiceWhenSellInIs0() {
+        product = Product.builder().sellIn(0).quality(10).build();
+        shop.update(product);
+        Assertions.assertEquals(8, product.getQuality());
+    }
+
+    @Test
+    void testUpdateShouldRaiseNegativeQualityExceptionWhenQualityIsNegative() {
+        product = Product.builder().quality(-10).sellIn(5).build();
+        Assertions.assertThrowsExactly(NegativeQualityException.class, () -> {
+            shop.update(product);
+        });
+    }
+
+    @Test
+    void testUpdateShouldDecreaseQualityOnceWhenSellInIs0AndQualityIs1() {
+        product = Product.builder().sellIn(0).quality(1).build();
+        shop.update(product);
+        Assertions.assertEquals(0, product.getQuality());
+    }
+
+    @Test
+    void testUpdateShouldRaiseExceptionWhenQualityGT50() {
+        product = Product.builder().sellIn(10).quality(51).build();
+        Assertions.assertThrowsExactly(MaxQualityException.class, () -> {
+            shop.update(product);
+        });
+    }
+
+    @Test
+    void testUpdateShouldIncreaseQualityWhenProductNameIsBrie() {
+        product = Product.builder().type("laitier").name("brie vieilli")
+                .sellIn(5).quality(10).build();
+        shop.update(product);
+        Assertions.assertEquals(11, product.getQuality());
+    }
+
+    @Test
+    void testUpdateShouldDecreaseQualityTwiceWhenTypeIsLaitier() {
+        product = Product.builder().sellIn(5).quality(10).type("laitier").build();
+        shop.update(product);
+        Assertions.assertEquals(8, product.getQuality());
+    }
+
+    @Test
+    void testUpdateShouldDecreaseQualityOnceWhenTypeIsLaitierAndQualityIs1() {
+        product = Product.builder().sellIn(5).type("laitier").quality(1).build();
+        shop.update(product);
+        Assertions.assertEquals(0, product.getQuality());
+    }
+
+    @Test
+    void testUpdateShouldDecreaseQualityFourTimeWhenTypeIsLaitierAndSellInIs0() {
+        product = Product.builder().sellIn(0).quality(10).type("laitier").build();
+        shop.update(product);
+        Assertions.assertEquals(6, product.getQuality());
+    }
+
+    @Test
+    void testUpdateShouldDecreaseQualityThreeTimeWhenTypeIsLaitierAndSellInIs0AndQualityIs3() {
+        product = Product.builder().sellIn(0).quality(3).type("laitier").build();
+        shop.update(product);
+        Assertions.assertEquals(0, product.getQuality());
+    }
+}
